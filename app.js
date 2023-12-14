017360962161ce57ff48e9502cdbcad0330422d3
christian <habinezac15@gmail.com>
2023-12-14 14:51:44 +0200

    get tours added

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    message: 'Hello from the server side!',
-    app: 'Natours',
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
   });
 });
 
