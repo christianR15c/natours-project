dee9429a516f2adb53594b290fcbdd9cae12317d
Christian <habinezac15@gmail.com>
2023-12-19 14:32:25 +0200

    ft: param middleware added

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,11 @@ const {
 
 const router = express.Router();
 
+router.param('tourId', (req, res, next, val) => {
+  console.log(`tour id is ${val}`);
+  next();
+});
+
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:tourId').get(getTour).patch(updateTour).delete(deleteTour);
 
