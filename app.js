8a87d28e9a9bdac0c65857912959f87b875d854f
Christian <habinezac15@gmail.com>
2023-12-19 11:33:28 +0200

    middlewares added

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const fs = require('fs');
+const morgan = require('morgan');
 
 const app = express();
 
+// Middlewares
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware...');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// Route handlers
 const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -22,9 +37,9 @@ const getAllTours = (req, res) => {
 const getTour = (req, res) => {
   const { tourId } = req.params;
 
-  if (tourId > tours.length || !tour)
+  if (tourId > tours.length || !tourId)
     return res.status(404).json({ status: 'fail', message: 'Invalid id' });
-  const tour = tours.find((tour) => tour.id === tourId * 1);
+  let tour = tours.find((tour) => tour.id === tourId * 1);
   res.status(200).json({
     status: 'success',
     data: {
@@ -78,6 +93,7 @@ const deleteTour = (req, res) => {
   });
 };
 
+// Routes
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:tourId')
@@ -89,6 +105,7 @@ app.post('/', (req, res) => {
   res.send('You can post using this endpoint...');
 });
 
+// Start the server
 const port = 3000;
 app.listen(port, () => {
   console.log(`server listening on port: ${port}...`);
