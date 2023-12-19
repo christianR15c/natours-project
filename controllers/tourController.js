da14c407afc5de8e7b4738ce8b511f3a39a60405
Christian <habinezac15@gmail.com>
2023-12-19 14:19:13 +0200

    ft: best practice file structure added

---
+++ controllers/tourController.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+const getTour = (req, res) => {
+  const { tourId } = req.params;
+
+  if (tourId > tours.length || !tourId)
+    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
+  let tour = tours.find((tour) => tour.id === tourId * 1);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  newTour = Object.assign({ id: newId }, req.body);
+
+  console.log(req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  const { tourId } = req.params;
+
+  let tour = tours.find((tour) => tour.id === tourId * 1);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  const { tourId } = req.params;
+
+  let tour = tours.find((tour) => tour.id === tourId * 1);
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+module.exports = {
+  getAllTours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+};
