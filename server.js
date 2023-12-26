f68317a34267af4acca3c56aa164a47e4c3ba90d
christian <habinezac15@gmail.com>
2023-12-26 14:04:44 +0200

    app connected to database

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const databaseUrl = process.env.MONGODB_URL;
+
+mongoose
+  .connect(databaseUrl)
+  .then((connection) => console.log('DB connected successfully!!!...'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
